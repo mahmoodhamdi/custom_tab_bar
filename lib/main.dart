b7501eb1b46aeee744adcbf66ed640a4737d8418
mahmoodhamdi <mahmouashwah@gmail.com>
2023-12-31 11:27:21 +0200

    custom animated tab_bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:custom_tab_bar/views/custom_tab_bar_view.dart';
+import 'package:custom_tab_bar/widgets/custom_tab_bar.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,11 +8,21 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return   MaterialApp(
-      home: CustomTabBar(),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Custom TabBar',
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              Expanded(child: CustomTabBar()),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
+
---
+++ lib/widgets/custom_tab_bar.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+
+class CustomTabBar extends StatefulWidget {
+  const CustomTabBar({super.key});
+
+  @override
+  State<CustomTabBar> createState() => _CustomTabBarState();
+}
+
+class _CustomTabBarState extends State<CustomTabBar> {
+// This widget is the root of your application.
+  List<String> tabs = [
+    "Popular",
+    "Most Visited",
+    "Recent",
+    "Explore",
+  ];
+  int current = 0;
+
+  double changePositionedOfLine() {
+    switch (current) {
+      case 0:
+        return 0;
+      case 1:
+        return 78;
+      case 2:
+        return 192;
+      case 3:
+        return 263;
+      default:
+        return 0;
+    }
+  }
+
+  double changeContainerWidth() {
+    switch (current) {
+      case 0:
+        return 50;
+      case 1:
+        return 80;
+      case 2:
+        return 50;
+      case 3:
+        return 50;
+      default:
+        return 0;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return   SizedBox(
+        width: size.width,
+        height: size.height,
+        child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.only(top: 15),
+              width: size.width,
+              height: size.height * 0.05,
+              child: Stack(
+                children: [
+                  Positioned(
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    child: SizedBox(
+                      width: size.width,
+                      height: size.height * 0.04,
+                      child: ListView.builder(
+                          physics: const BouncingScrollPhysics(),
+                          scrollDirection: Axis.horizontal,
+                          itemCount: tabs.length,
+                          itemBuilder: (context, index) {
+                            return Padding(
+                              padding: EdgeInsets.only(
+                                  left: index == 0 ? 10 : 23, top: 7),
+                              child: GestureDetector(
+                                onTap: () {
+                                  setState(() {
+                                    current = index;
+                                  });
+                                },
+                                child: Text(
+                                  tabs[index],
+                                  style: TextStyle(
+                                    fontSize: current == index ? 16 : 14,
+                                    fontWeight: current == index
+                                        ? FontWeight.w400
+                                        : FontWeight.w300,
+                                  ),
+                                ),
+                              ),
+                            );
+                          }),
+                    ),
+                  ),
+                  AnimatedPositioned(
+                    curve: Curves.fastLinearToSlowEaseIn,
+                    bottom: 0,
+                    left: changePositionedOfLine(),
+                    duration: const Duration(milliseconds: 500),
+                    child: AnimatedContainer(
+                      margin: const EdgeInsets.only(left: 10),
+                      width: changeContainerWidth(),
+                      height: size.height * 0.008,
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurpleAccent,
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      duration: const Duration(milliseconds: 1000),
+                      curve: Curves.fastLinearToSlowEaseIn,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: size.height * 0.3),
+              child: Text(
+                "${tabs[current]} Tab Content",
+                style: const TextStyle(fontSize: 30),
+              ),
+            )
+          ],
+        ),
+       
+    );
+  }
+}
