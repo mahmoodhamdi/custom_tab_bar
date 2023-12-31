9187055b0494b426a145c9fe36ae1b0c29461200
mahmoodhamdi <mahmouashwah@gmail.com>
2023-12-31 10:23:54 +0200

    first commit

---
+++ lib/views/widgets/custom_tab_bar_view_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CustomTabBarViewBody extends StatelessWidget {
+  const CustomTabBarViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
